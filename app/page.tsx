797aa9db7cc226249fc012e311bf9587f44e276c
Jason Lin <jasonlin@brandeis.edu>
2024-09-12 18:23:09 -0400

    added toc

--- app/page.tsx
+++ app/page.tsx
@@ -51,6 +51,21 @@ export default function Home() {
             Learn more about our privacy policy.
           </p>
         </Link>
+
+        <Link
+          href="/policy/terms-and-conditions"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            About{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn more about our terms and conditions.
+          </p>
+        </Link>
       </div>
     </main>
   );
