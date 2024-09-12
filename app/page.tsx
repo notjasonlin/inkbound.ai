0a206ab3cf46ff59a1946137066a7d3e93671c97
Jason Lin <jasonlin@brandeis.edu>
2024-09-12 18:08:41 -0400

    added privacy policy

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
         </Link>
 
         <Link
-          href="/about"
+          href="/policy/privacy"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -48,7 +48,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn more about our college athlete program.
+            Learn more about our privacy policy.
           </p>
         </Link>
       </div>
