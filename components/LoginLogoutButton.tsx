6f1f7047af6de2edb6aa08a060d71b438f41116e
elan-romo <elanromo@brandeis.edu>
2024-09-13 12:13:23 -0400

    Landing page

--- components/LoginLogoutButton.tsx
+++ components/LoginLogoutButton.tsx
@@ -1,44 +1,41 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
-import { signout } from "@/lib/auth-actions";
 
-const LoginButton = () => {
+const LoginButton = ({ label }: { label?: string }) => {
   const [user, setUser] = useState<any>(null);
   const router = useRouter();
   const supabase = createClient();
+
   useEffect(() => {
     const fetchUser = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
       setUser(user);
+
+      // Redirect to dashboard if user is logged in
+      if (user) {
+        router.push('/dashboard');
+      }
     };
     fetchUser();
-  }, []);
+  }, [supabase.auth, router]);
+
   if (user) {
-    return (
-      <Button
-        onClick={() => {
-          signout();
-          setUser(null);
-        }}
-      >
-        Log out
-      </Button>
-    );
+    return null; // No need for a login button if the user is already logged in
   }
+
   return (
-    <Button
-      variant="outline"
+    <button
+      className="px-4 py-2 font-semibold text-white bg-babyblue-600 rounded-lg hover:bg-babyblue-700"
       onClick={() => {
-        router.push("/login");
+        router.push("/login"); // Redirect to login page
       }}
     >
-      Login
-    </Button>
+      {label ? label : "Login"}
+    </button>
   );
 };
 
