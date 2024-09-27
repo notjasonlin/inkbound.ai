54a9af8c3ab39edbca8a7090620dd7c12cfd9c52
Jason Lin <113144409+notjasonlin@users.noreply.github.com>
2024-09-27 17:05:31 -0400

    Update EmailSender.tsx

--- app/dashboard/compose/components/EmailSender.tsx
+++ app/dashboard/compose/components/EmailSender.tsx
@@ -116,18 +116,6 @@ const EmailSender: React.FC<EmailSenderProps> = ({ school, onEmailSent, setIsOpe
     }
   };
 
-
-  const writeWithTemplate = () => {
-    const updatedDraft = {
-      emailsTo: school?.coaches?.map(coach => coach.email).join(', ') || '',
-      subject: selectedTemplate.content.title,
-      template: selectedTemplate.title,
-      customSection: readTemplate(selectedTemplate, school),
-    }
-    setEmailDraft(updatedDraft);
-    debouncedSave(updatedDraft);
-  }
-
   const writeWithTemplate = () => {
     if (selectedTemplate) {
       const updatedDraft = {
