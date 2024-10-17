b1af598cc2ba8e4f18f9cb11bed2198a0643caf3
Jason Lin <jasonlin@brandeis.edu>
2024-10-16 20:31:54 -0400

    fixed template for compose

--- functions/readTemplate.tsx
+++ functions/readTemplate.tsx
@@ -1,8 +1,8 @@
 import { SchoolData } from "@/types/school";
 import { TemplateData } from "@/types/template";
 
-const COACH = '<span class=\"bg-blue-100 text-blue-800 px-2 py-1 rounded-full inline-block\" contenteditable=\"false\" data-placeholder=\"[coachLastName]\">Coach</span>';
-const SCHOOL = '<span class=\"bg-blue-100 text-blue-800 px-2 py-1 rounded-full inline-block\" contenteditable=\"false\" data-placeholder=\"[schoolName]\">School Name</span>';
+const COACH = '[coachLastName]';
+const SCHOOL = '[schoolName]';
 
 export default function readTemplate(template: TemplateData, school?: SchoolData) {
     let content = template.content.content;
@@ -10,16 +10,18 @@ export default function readTemplate(template: TemplateData, school?: SchoolData
     if (content) {
         const placeholders: string[] = [COACH, SCHOOL];
         let replacements: string[];
-        if (school) {
-            replacements = ["Coach " + school.coaches[0].name, school.school]
+        if (school && school.coaches && school.coaches.length > 0) {
+            const coachFullName = school.coaches[0].name;
+            const coachLastName = coachFullName.split(' ').pop() || '';
+            replacements = [`Coach ${coachLastName}`, school.school];
         } else {
-            replacements = ["Coach [BLANK]", "[BLANK] University"]
+            replacements = ["Coach [BLANK]", "[BLANK] University"];
         }
 
-        let len = placeholders.length;
-        for (let i = 0; i < len; i++) {
-            content = content.replaceAll(placeholders[i], replacements[i]);
-        }
+        placeholders.forEach((placeholder, index) => {
+            content = content.replaceAll(placeholder, replacements[index]);
+        });
+        
         return content;
     }
 }
