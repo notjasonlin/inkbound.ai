07148263adc0895cc65916e303e7dde62e00c891
notjasonlin <linyongkang9@gmail.com>
2024-10-31 16:47:30 -0400

    added some more vercel connections

--- middleware.ts
+++ middleware.ts
@@ -39,9 +39,10 @@ export async function middleware(request: NextRequest) {
     script-src-elem 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'nonce-${nonce}';
     style-src-elem 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
+    img-src 'self' https://vercel.live/ https://vercel.com *.pusher.com/ data: blob:;
     font-src 'self' https://fonts.gstatic.com https://*.vercel.live;
-    connect-src 'self' https://tvjclbhclyozgziixpcp.supabase.co wss://tvjclbhclyozgziixpcp.supabase.co https://api.openai.com https://accounts.google.com https://vitals.vercel-insights.com;
+    connect-src 'self' https://tvjclbhclyozgziixpcp.supabase.co wss://tvjclbhclyozgziixpcp.supabase.co https://api.openai.com https://accounts.google.com https://vitals.vercel-insights.com https://vercel.live/ https://vercel.com *.pusher.com *.pusherapp.comhttps://vercel.live/ https://vercel.com *.pusher.com *.pusherapp.com;
+    frame-src 'self' https://vercel.live/ https://vercel.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
